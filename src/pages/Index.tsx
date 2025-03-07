d87beb73c9918de10a9b9aa907c77b900050a047
movildriveia <45123221+movildriveia@users.noreply.github.com>
2025-03-07 03:51:12 +0100

    Update Index.tsx
    chng

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -40,7 +40,7 @@ const Index = () => {
                 size="sm"
                 onClick={() => navigate("/register")}
               >
-                Register
+                Sign-Upsss
               </Button>
             </div>
           </div>
